d98fc1b345cb72f8bfef44ed03f17ae6772f7006
pek9931937-glitch <pek9931937@gmail.com>
2025-11-18 23:46:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# 메인 페이지
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
-# 민재 페이지
 @app.route("/minjae")
 def minjae():
     return render_template("minjae.html")
