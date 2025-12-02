4e28ca346b66e3778b53e3871b3991121bba77b8
pek9931937-glitch <pek9931937@gmail.com>
2025-12-03 00:32:10 +0900

    Update popup.js

--- static/popup.js
+++ static/popup.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const teacher = document.getElementById("teacher-photo");
+    const popup = document.getElementById("popup");
+    const popupImg = document.getElementById("popup-img");
+
+    teacher.addEventListener("click", () => {
+        popup.style.display = "flex";
+        popupImg.src = teacher.src;
+    });
+
+    popup.addEventListener("click", () => {
+        popup.style.display = "none";
+    });
+});
 
